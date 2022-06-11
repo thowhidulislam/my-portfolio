ddeba8e9e56c9a396b1c8988421b2e272524f6c1
thowhidulislam <thowhid.ndc14@gmail.com>
2022-06-11 16:15:14 +0600

    changed footer font size

--- src/components/Shared/Footer.js
+++ src/components/Shared/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='bg-neutral py-5'>
-            <h1 className='text-info text-md text-center'>Designed and Built by Thowhidul Islam.</h1>
+            <p className='text-info text-xs text-center'>Designed and Built by Thowhidul Islam.</p>
         </div>
     );
 };
