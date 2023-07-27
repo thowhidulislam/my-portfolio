a502c62f2bf835dd33b9f26d2c9ca2fa85c778f1
Md. Thowhidul Islam Molla <96806972+thowhidulislam@users.noreply.github.com>
2023-07-27 20:40:51 +0600

    updated

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import portfolio from '../../assets/portfolio/Md Thowhidul Islam Molla.pdf'
-import PrimaryButton from '../Shared/PrimaryButton';
-import LinksBar from '../LinksBar/LinksBar';
+import React from "react";
+import portfolio from "../../assets/portfolio/Md Thowhidul Islam Molla.pdf";
+import LinksBar from "../LinksBar/LinksBar";
+import PrimaryButton from "../Shared/PrimaryButton";
 
 const Hero = () => {
-    return (
-        <section className='relative'>
-            <div class="py-8 lg:py-32 min-w-screen">
-                <div class="flex-col lg:flex-row">
-                    <div className=''>
-                        <h1 class="text-xl font-bold text-base-100 my-3 tracking-widest">Hi, my name is </h1>
-                        <h1 className='text-5xl lg:text-8xl font-bold text-primary'>Thowhidul Islam</h1>
-                        <p class="py-6 text-info">I am an aspiring web developer based in the Bangladesh.</p>
-                        <LinksBar></LinksBar>
-                        <div className='button-group-custom relative my-7'>
-                            <a href={portfolio} download='Md Thowhidul Islam Molla.pdf'>
-                                <PrimaryButton>Resume</PrimaryButton>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="relative">
+      <div class="py-8 lg:py-32 min-w-screen">
+        <div class="flex-col lg:flex-row">
+          <div className="">
+            <h1 class="text-xl font-bold text-base-100 my-3 tracking-widest">
+              Hi, my name is{" "}
+            </h1>
+            <h1 className="text-5xl lg:text-8xl font-bold text-primary">
+              Thowhidul Islam
+            </h1>
+            <p class="py-6 text-info">
+              Front-end web developer with one year of experience, skilled in
+              various technologies, including React, and highly interested in
+              the MERN stack.
+            </p>
+            <LinksBar></LinksBar>
+            <div className="button-group-custom relative my-7">
+              <a href={portfolio} download="Md Thowhidul Islam Molla.pdf">
+                <PrimaryButton>Resume</PrimaryButton>
+              </a>
             </div>
-        </section >
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/components/LinksBar/LinksBar.js
+++ src/components/LinksBar/LinksBar.js
@@ -1,19 +1,60 @@
-import React from 'react';
+import React from "react";
 
 const LinksBar = () => {
-    return (
-        <section className='flex flex-row gap-x-3 mb-12'>
-            {/* Github link */}
-            <div>
-                <a href='https://github.com/thowhidulislam' className='text-info focus:bg-transparent hover:text-primary font-bold' target='_blank' rel='noreferrer'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a>
-            </div>
-            {/* LinkedIn link */}
-            <div>
-                <a href='https://www.linkedin.com/in/thowhidulislam/' target='_blank' rel="noreferrer" className='text-info focus:bg-transparent hover:text-primary font-bold'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg></a>
-            </div>
-
-        </section>
-    );
+  return (
+    <section className="flex flex-row gap-x-3 mb-12">
+      {/* Github link */}
+      <div>
+        <a
+          href="https://github.com/thowhidulislam"
+          className="text-info focus:bg-transparent hover:text-primary font-bold"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-github"
+          >
+            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+          </svg>
+        </a>
+      </div>
+      {/* LinkedIn link */}
+      <div>
+        <a
+          href="https://www.linkedin.com/in/thowhidulislam/"
+          target="_blank"
+          rel="noreferrer"
+          className="text-info focus:bg-transparent hover:text-primary font-bold"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-linkedin"
+          >
+            <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+            <rect x="2" y="9" width="4" height="12"></rect>
+            <circle cx="4" cy="4" r="2"></circle>
+          </svg>
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default LinksBar;
